c27d418c63f52608c979ba652b7c460eecd2fa05
Sisi Small <smallsianah@gmail.com>
2023-07-27 09:59:39 -0500

    added code to App.js and initial storage

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet}from 'react-native';
+import React from 'react';
+import AppNavigator from './AppNavigator';
+import { Provider as PaperProvider } from 'react-native-paper';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <AppNavigator />
+    </PaperProvider>
   );
 }
 
