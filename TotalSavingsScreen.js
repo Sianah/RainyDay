d390075b799a655b87404faac910b4aaaf944b61
Sisi Small <smallsianah@gmail.com>
2023-07-28 10:34:13 -0500

    added cute tiny cactus bg

--- TotalSavingsScreen.js
+++ TotalSavingsScreen.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { ImageBackground } from 'react-native';
 
 const TotalSavingsScreen = ({ route }) => {
   const goals = route.params?.goals || [];
   const totalSavings = goals.reduce((sum, goal) => sum + parseFloat(goal.amountRequired), 0);
 
   return (
+    <ImageBackground source={require('./tinyCactus.jpg')} style={{ flex: 1 }}>
     <View style={styles.container}>
       <Text style={styles.text}>Total Savings Required: ${totalSavings.toFixed(2)}</Text>
     </View>
+    </ImageBackground>
   );
 };
 
@@ -21,6 +24,7 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 18,
     fontWeight: 'bold',
+    color: 'white'
   },
 });
 
